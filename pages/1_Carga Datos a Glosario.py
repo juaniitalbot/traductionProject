6b374bcf577c3cb597817d551518205245222870
juaniitalbot <juanignaciotalbot@gmail.com>
2023-07-26 15:47:57 -0300

    Actualizacion

--- pages/1_Carga Datos a Glosario.py
+++ pages/1_Carga Datos a Glosario.py
@@ -12,10 +12,7 @@ st.set_page_config(
 
 # Configurar la conexión a Firebase
 cred = credentials.Certificate("traductionproject-efb4b-firebase-adminsdk-ms2w0-a9b0b2055d.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://traductionproject-efb4b-default-rtdb.firebaseio.com/'
-})
-
+firebase_admin.initialize_app(cred)
 # Función para insertar DataFrame en la colección "glosario" de Firebase
 def insert_dataframe_to_db(dataframe):
     try:
